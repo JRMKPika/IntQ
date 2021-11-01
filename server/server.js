f14b784ba6aad6dbee5129535b08c5d14a4de3e0
mkalaizic <kalaizicmercedes@gmail.com>
2021-11-01 15:30:39 -0400

    first commit with webpack!

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = 3000; 
+
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.json());
+
+
+app.use("/build", express.static(path.join(__dirname, "../build")));
+
+
+app.get('/', (req,res)=> {
+  return res.status(200).sendFile(path.join(__dirname, "../client/index.html"));
+});
+
+app.listen(PORT, ()=> {
+  console.log(`The server is on on port ${PORT}. It's listening...`); 
+});
+
