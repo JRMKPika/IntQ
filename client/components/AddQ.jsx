5d3fbb31c502f97bdb4a7fadd9a8269a002f810d
mkalaizic <kalaizicmercedes@gmail.com>
2021-11-02 15:53:17 -0400

    addQ progress!

--- client/components/AddQ.jsx
+++ client/components/AddQ.jsx
@@ -1,8 +1,33 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 function AddQ() {
+  const history = useHistory();
   function addQuestion() {
-    
+    const question = document.querySelector('#question').value;
+    const company = document.querySelector('#company').value;
+    const questionTypes = document.querySelector('#questionTypes').value;
+    const rol = document.querySelector('#rol').value;
+
+    fetch(`/addQuestion`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ question, company, questionTypes, rol }),
+    }).catch((err) => console.log('Error while adding question: ', err));
+  }
+  async function AddAndContinue() {
+    await addQuestion();
+    document.querySelector('#question').value = '';
+    document.querySelector('#company').value = '';
+    document.querySelector('#questionTypes').value = '';
+    document.querySelector('#rol').value = '';
+    window.alert('Your question was succesfully added.');
+  }
+
+  async function AddAndFinish() {
+    await addQuestion();
+    history.goBack();
+    window.alert('Your question was succesfully added.');
   }
 
   return (
@@ -11,11 +36,11 @@ function AddQ() {
       <form>
         <div className='form'>
           <label>Question: </label>
-          <input placeholder='i.e. What is a promise?'></input>
+          <input id='question' placeholder='i.e. What is a promise?'></input>
           <label>Company: </label>
-          <input placeholder='i.e. Google'></input>
+          <input id='company' placeholder='i.e. Google'></input>
           <label>Question type: </label>
-          <select id="questionTypes">
+          <select id='questionTypes'>
             <option value='algo'>Algo</option>
             <option value='behavioral'>Behavioral</option>
             <option value='general'>General</option>
@@ -23,12 +48,15 @@ function AddQ() {
             <option value='technology'>Technology</option>
           </select>
           <label>Rol: </label>
-          <input placeholder='i.e. Front End Senior Software Engineer'></input>
+          <input
+            id='rol'
+            placeholder='i.e. Front End Senior Software Engineer'
+          ></input>
         </div>
       </form>
       <div className='submitButtons'>
-      <button onClick={addQuestion} >Submit and finish</button>
-      <button onClick={addQuestion} >Submit and continue adding</button>
+        <button onClick={AddAndFinish}>Submit and finish</button>
+        <button onClick={AddAndContinue}>Submit and continue adding</button>
       </div>
     </div>
   );
