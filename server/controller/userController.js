cb16a01f6626264221bcfa5e0b720a55aa6743a9
Kasthuri Menon <kasthuri.menon1@gmail.com>
2021-11-03 12:15:39 -0400

    timestamp added

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -119,7 +119,7 @@ userController.AddQuestion = async (req, res, next) => {
     console.log("the req body is ",req.body);
     const {question, company, questionTypes, role, googleId, username } = req.body;
 
-    const insertQuery = `INSERT INTO questions VALUES (DEFAULT,'${question}','${company}','${questionTypes}','${role}',CURRENT_DATE,${googleId})`;
+    const insertQuery = `INSERT INTO questions VALUES (DEFAULT,'${question}','${company}','${questionTypes}','${role}',CURRENT_TIMESTAMP,${googleId})`;
 
     db.query(insertQuery)
     .then( (data) => {
