e7f42fa248f358aeb814b4a9e51c75cd82c37a22
Kasthuri Menon <kasthuri.menon1@gmail.com>
2021-11-03 11:14:47 -0400

    new

--- server/routes.js
+++ server/routes.js
@@ -3,16 +3,41 @@ const router = express.Router();
 
 const userController = require('./controller/userController');
 
-app.post(`/loginUser`, userController.signIn, (req, res) => {
+
+router.post('/addQuestion',
+ userController.AddQuestion,
+  (req,res) => {
+    retuen res.status(200).json(res.locals.user);
+  });
+
+router.get('/search/type/:searchText',
+userController.searchByType,
+(req, res) => {
+  return res.status(200).json(res.locals.searchResults);
+});
+
+router.get('/search/role/:searchText',
+userController.searchByRole,
+(req, res) => {
+  return res.status(200).json(res.locals.searchResults);
+});
+
+router.get('/search/company/:searchText',
+userController.searchByCompany,
+(req, res) => {
+return res.status(200).json(res.locals.searchResults);
+});
+
+router.post(`/loginUser`, userController.signIn, (req, res) => {
   res.status(200).json(res.locals.addedUser);
 });
 
 router.get('/newestTen', userController.newestTen, (req, res) => {
-  return res.status(200).send(res.locals.newestTen)
+  return res.status(200).send(res.locals.newestTen);
 })
 
 router.get('/allQ', userController.allQ, (req, res) => {
-  return res.status(200).send(res.locals.allQ)
+  return res.status(200).send(res.locals.allQ);
   })
 
   //this might be a put...
@@ -23,6 +48,10 @@ router.put('/myQ', userController.myQ, (req, res) => {
 router.delete('/deleteQ', userController.deleteQ, (req, res) => {
     return res.status(200).send(res.locals.deleteQ)
   })
+
+router.get('/', (req,res)=> {
+    return res.status(200).sendFile(path.join(__dirname, "../client/index.html"));
+  });
 
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = 3000;
-const routes = require('./routes')
+let sessionId;
 
 const userController = require('./controller/userController.js')
+
 
 app.use(express.urlencoded({extended: true}));
 
@@ -12,41 +13,10 @@ app.use(express.json());
 
 app.use('/build', express.static(path.join(__dirname, '../build')));
 
-app.use("/build", express.static(path.join(__dirname, "../build")));
-
-app.get('/search/company/:searchText',
-userController.searchByCompany,
-(req, res) => {
-return res.status(200).json(res.locals.searchResults);
-});
-
-app.get('/search/role/:searchText',
-userController.searchByRole,
-(req, res) => {
-  return res.status(200).json(res.locals.searchResults);
-});
-
-app.get('/search/type/:searchText',
-userController.searchByType,
-(req, res) => {
-  return res.status(200).json(res.locals.searchResults);
-});
-
-app.post('/addQuestion',
- userController.AddQuestion,
-  (req,res) => {});
-
-app.post(`/loginUser`,
-userController.signIn,
-(req, res) => {
-  res.status(200).json(res.locals.addedUser);
-});
 
 app.get('/', (req,res)=> {
   return res.status(200).sendFile(path.join(__dirname, "../client/index.html"));
 });
-
-app.use('/', routes);
 
 app.use((err, req, res, next) => {
   const defaultErr = {
@@ -62,5 +32,6 @@ app.use((err, req, res, next) => {
 app.listen(PORT, ()=> {
   console.log(`The server is on on port ${PORT}. It's listening...`); 
 });
+
 
 module.exports = app;
