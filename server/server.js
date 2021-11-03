1078cf21ec12ab68f4ed6ed6ef1e13bb37a6f491
Kasthuri Menon <kasthuri.menon1@gmail.com>
2021-11-03 11:24:08 -0400

    routing issue

--- server/server.js
+++ server/server.js
@@ -13,10 +13,58 @@ app.use(express.json());
 
 app.use('/build', express.static(path.join(__dirname, '../build')));
 
+app.use("/build", express.static(path.join(__dirname, "../build")));
+
+app.post('/addQuestion',
+ userController.AddQuestion,
+  (req,res) => {
+    return res.status(200).json(res.locals.user);
+  });
+
+app.get('/search/type/:searchText',
+userController.searchByType,
+(req, res) => {
+  return res.status(200).json(res.locals.searchResults);
+});
+
+app.get('/search/role/:searchText',
+userController.searchByRole,
+(req, res) => {
+  return res.status(200).json(res.locals.searchResults);
+});
+
+app.get('/search/company/:searchText',
+userController.searchByCompany,
+(req, res) => {
+return res.status(200).json(res.locals.searchResults);
+});
+
+app.post(`/loginUser`, userController.signIn, (req, res) => {
+  res.status(200).json(res.locals.addedUser);
+});
+
+app.get('/newestTen', userController.newestTen, (req, res) => {
+  return res.status(200).send(res.locals.newestTen);
+})
+
+app.get('/allQ', userController.allQ, (req, res) => {
+  return res.status(200).send(res.locals.allQ);
+  })
+
+  //this might be a put...
+app.put('/myQ', userController.myQ, (req, res) => {
+  return res.status(200).send(res.locals.myQ)
+})
+
+app.delete('/deleteQ', userController.deleteQ, (req, res) => {
+    return res.status(200).send(res.locals.deleteQ)
+  })
 
 app.get('/', (req,res)=> {
-  return res.status(200).sendFile(path.join(__dirname, "../client/index.html"));
-});
+    return res.status(200).sendFile(path.join(__dirname, "../client/index.html"));
+  });
+
+// app.use('/', routes);
 
 app.use((err, req, res, next) => {
   const defaultErr = {
