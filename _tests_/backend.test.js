6a062117059bffa666632cb8b623b9f450dfb06a
crossjp1 <Jamespvcross@gmail.com>
2021-11-03 10:51:09 -0400

    added test files

---
+++ _tests_/backend.test.js
@@ -0,0 +1,13 @@
+// const fs = require('fs');
+// const path = require('path');
+// const request = require('supertest');
+
+// const testJsonFile = path.join(__dirname, '')
+
+// const server = 'http://localhost:3000';
+
+describe('Sample Test', () => {
+    it('should test that true === true', () => {
+      expect(true).toBe(true)
+    })
+  })
